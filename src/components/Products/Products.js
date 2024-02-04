cd99e42848f551d036b9d6be36699646af4816c2
Ankit Tomar <ankitjdec@gmail.com>
2024-02-04 17:11:30 +0530

    made the boot strap card and the dynamic content usibg the stateless components

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,39 +4,38 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ListItems from "./ListItems.js/ListItems";
 const Products = () => {
+  const items = [
+    {
+      image: "placeholder.png",
+      price: "450",
+      discountedPrice: "340",
+      title: "Title 1",
+    },
+    {
+      image: "placeholder.png",
+      price: "200",
+      discountedPrice: "150",
+      title: "Title 2",
+    },
+    {
+      image: "placeholder.png",
+      price: "650",
+      discountedPrice: "500",
+      title: "Title 3",
+    },
+  ];
   return (
     <div>
       <Container>
         <Row>
           <Col>
-            <ListItems
-              data={{
-                image: "placeholder.png",
-                price: "450",
-                discountedPrice: "340",
-                title: "Title 1",
-              }}
-            />
+            <ListItems data={items[0]} />
           </Col>
           <Col>
-            <ListItems
-              data={{
-                image: "placeholder.png",
-                price: "200",
-                discountedPrice: "150",
-                title: "Title 2",
-              }}
-            />
+            <ListItems data={items[1]} />
           </Col>
           <Col>
-            <ListItems
-              data={{
-                image: "placeholder.png",
-                price: "650",
-                discountedPrice: "500",
-                title: "Title 3",
-              }}
-            />
+            <ListItems data={items[2]} />
           </Col>
         </Row>
       </Container>
