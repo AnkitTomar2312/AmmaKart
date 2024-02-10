ffbb80a75fa3355bc4b55a438069abc68921a8a1
Ankit Tomar <ankitjdec@gmail.com>
2024-02-10 19:26:10 +0530

    made cards dynamic

--- src/components/ListItems.js/ListItems.js
+++ src/components/ListItems.js/ListItems.js
@@ -1,25 +1,30 @@
 import React from "react";
-const ListItems = () => {
+const ListItems = ({ data }) => {
   return (
     <div className={"item-card"}>
       <img
         className={"img-fluid"}
-        src="./assets/images/placeholder.png"
+        src={`./assets/images/${data.image}`}
         alt="placeholder"
       />
       <div className={"item-card__information"}>
         <div className={"pricing"}>
-          <span>Rs. 360</span>
+          <span>{data.discountedPrice}</span>
           <small>
-            <strike>Rs. 450</strike>
+            <strike>{data.price}</strike>
           </small>
         </div>
         <div className={"title"}>
-          <h3>Title of the Item</h3>
+          <h3>{data.title}</h3>
         </div>
       </div>
 
-      <button className={"cart-add"}>
+      <button
+        className={"cart-add"}
+        // onClick={() => {
+        //   data;
+        // }}
+      >
         <span>Add to Cart</span>
         <img src="./assets/icons/add_cart.svg" alt="addtocartsvg" />
       </button>
