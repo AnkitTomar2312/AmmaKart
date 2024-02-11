b11bd450bf705d5fe39f1c28a22c914fa360b5fc
Ankit Tomar <ankitjdec@gmail.com>
2024-02-11 19:51:41 +0530

    state uplifting

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ListItems from "../ListItems.js/ListItems";
+import Form from "./Form";
 function ProductForm() {
   const [title, setTitle] = useState("Title of Item");
   const [price, setPrice] = useState(0);
@@ -17,68 +18,20 @@ function ProductForm() {
 
   const handleForm = (e) => {
     e.preventDefault();
-    setItem({
-      thumbnail: thumbnail,
-      title: title,
-      discountedPrice: discount,
-      price: price,
-    });
+    if (item.discountedPrice > item.price) {
+      alert("Discounted Price cannot be greater than price");
+      return;
+    }
+    console.log("Item Updated!", item);
   };
   return (
     <div className={"product-wrapper"}>
       <div className={"form"}>
-        <form>
-          <h2>Item Card Details</h2>
-          <div className={"input-field"}>
-            <label htmlFor="title">Title</label>
-            <input
-              name="title"
-              type="text"
-              placeholder="enter title..."
-              onChange={handleInput}
-              value={item.title}
-            />
-          </div>
-          <div className={"input-field"}>
-            <label htmlFor="price">Price</label>
-            <input
-              name="price"
-              type="number"
-              placeholder="0"
-              onChange={handleInput}
-              value={item.price}
-            />
-          </div>
-          <div className={"input-field"}>
-            <label htmlFor="price">Discounted Price</label>
-            <input
-              name="discountedPrice"
-              type="number"
-              placeholder="0"
-              onChange={handleInput}
-              value={item.discount}
-            />
-          </div>
-          <div className={"input-field"}>
-            <label htmlFor="price">Thumbnail</label>
-            <input
-              name="thumbnail"
-              type="text"
-              placeholder="0"
-              onChange={handleInput}
-              value={item.thumbnail}
-            />
-          </div>
-          <div className={"submit-wrap"}>
-            <button
-              onClick={(e) => {
-                handleForm(e);
-              }}
-            >
-              Update
-            </button>
-          </div>
-        </form>
+        <Form
+          item={item}
+          onChangeForm={handleInput}
+          onFormSubmit={handleForm}
+        />
       </div>
       <div className="TwoWayCart">
         <ListItems data={item} />
